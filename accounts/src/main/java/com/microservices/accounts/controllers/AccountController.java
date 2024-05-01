00644451bc19453b895ba9c3408f940c51e0ebd8
Kolotushka1 <maksim.kolotov.050605@mail.ru>
2024-05-01 21:47:23 +0700

    Start-of-project
    Start-of-project

---
+++ accounts/src/main/java/com/microservices/accounts/controllers/AccountController.java
@@ -0,0 +1,14 @@
+package com.microservices.accounts.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("hello")
+    public String sayHello() {
+        return "Hello world!";
+    }
+
+}
