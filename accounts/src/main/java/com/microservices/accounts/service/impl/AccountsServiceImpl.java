d98582191724212d32510363912271e2b62e5c3e
Kolotushka1 <maksim.kolotov.050605@mail.ru>
2024-05-05 02:07:49 +0700

    Handle-error+validation
    Handle-error+validation

--- accounts/src/main/java/com/microservices/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/microservices/accounts/service/impl/AccountsServiceImpl.java
@@ -21,7 +21,7 @@ import java.util.Random;
 
 @Service
 @AllArgsConstructor
-public class AccountsServiceImpl implements IAccountsService {
+public  class AccountsServiceImpl implements IAccountsService {
 
     private AccountsRepository accountsRepository;
     private CustomerRepository customerRepository;
@@ -103,6 +103,20 @@ public class AccountsServiceImpl implements IAccountsService {
             isUpdated = true;
         }
         return isUpdated;
+    }
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    @Override
+    public boolean deleteAccount(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                ()  -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        accountsRepository.deleteByCustomerId(customer.getCustomerId());
+        customerRepository.deleteById(customer.getCustomerId());
+        return true;
     }
 
 }
